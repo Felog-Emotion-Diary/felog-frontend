3c1020cfbed73b8688e310b3e223515598aded1b
jieun <jieunh0311@gmail.com>
2025-06-02 14:33:28 +0900

    잊고있던 하루 컴포넌트 구현

---
+++ src/components/mainComponent/RandomDiaryTab.tsx
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+
+function RandomDiaryTab() {
+  return (
+    <RandomDiaryTabStyle>
+      <h2 className="tab-title">잊고 있던 하루 💭</h2>
+    </RandomDiaryTabStyle>
+  );
+}
+
+const RandomDiaryTabStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  min-height: 60px;
+  background-color: #fffdf8;
+  text-align: center;
+  box-shadow: 0 -4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 20px 20px 0 0px;
+
+  .tab-title {
+    font-size: 16px;
+    font-weight: 500;
+    color: #333; 
+  }
+`;
+
+export default RandomDiaryTab;
