663cbf812d883ad0a965e018050849c2c0db969c
jieun <jieunh0311@gmail.com>
2025-06-05 14:04:24 +0900

    비밀번호 재설정 기능 추가

--- src/pages/CheckEmail.tsx
+++ src/pages/CheckEmail.tsx
@@ -1,18 +1,33 @@
 import { Button, FormContainer, FullPage, Input } from "../style/PasswordStyle";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios, { AxiosError } from "axios";
 
 function CheckEmail() {
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/reset-password");
-  }
+  const handleClick = async () => {
+    try {
+      await axios.post("http://localhost:5000/api/users/requestPasswordReset", { email });
+      alert("비밀번호 재설정 링크가 이메일로 전송되었습니다.");
+    } catch (error: unknown) {
+      const err = error as AxiosError<{ message: string }>;
+      alert(err.response?.data?.message || "에러가 발생했습니다");
+      navigate("/reset-password");
+    }
+  };
 
   return (
     <FullPage>
       <FormContainer>
         <h1>재설정 메일 확인</h1>
-        <Input type="email" placeholder="이메일" />
+        <Input
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <Button onClick={handleClick}>확인</Button>
       </FormContainer>
     </FullPage>
@@ -20,4 +35,3 @@ function CheckEmail() {
 }
 
 export default CheckEmail;
-
